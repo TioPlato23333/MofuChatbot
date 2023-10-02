1e9e0354f071fbdc7026da1b6d11623e3ff75740
TioPlato23333 <ningxiaodong23333@gmail.com>
2023-10-02 19:24:50 +0800

    Print tensors.

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ if __name__ == '__main__':
         # generated a response while limiting the total chat history to 1000 tokens, 
         chat_history_ids = model.generate(bot_input_ids, max_length=1000, \
             pad_token_id=tokenizer.eos_token_id)
+        print(bot_input_ids)
+        print(chat_history_ids)
         # pretty print last ouput tokens from bot
         print("DialoGPT: {}".format(tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], \
             skip_special_tokens=True)))
