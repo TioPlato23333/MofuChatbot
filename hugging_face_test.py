9be38339da9bac4551290a5f52db7ced0279069d
TioPlato23333 <ningxiaodong23333@gmail.com>
2023-10-06 19:06:20 +0800

    Try tokenizer.

--- hugging_face_test.py
+++ hugging_face_test.py
@@ -1,4 +1,15 @@
+from transformers import AutoTokenizer
 from transformers import pipeline
 
-generator = pipeline('text-generation', model='./DialoGPT-medium')
-print(generator('Tomorrow we will'))
+# generator = pipeline('text-generation', model='./DialoGPT-medium')
+# print(generator('Tomorrow we will'))
+
+checkpoint = './DialoGPT-medium'
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+tokenizer.pad_token = tokenizer.eos_token
+raw_inputs = [
+    'This is input 1',
+    'Hello, I\'m the second input'
+]
+inputs = tokenizer(raw_inputs, padding=True, truncation=True, return_tensors='pt')
+print(inputs)
