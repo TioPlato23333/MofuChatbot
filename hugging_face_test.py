f89e6e39cb2c86b08102759da4c6a4c306a07b60
TioPlato23333 <ningxiaodong23333@gmail.com>
2023-10-08 20:49:32 +0800

    Understand tokens.

--- hugging_face_test.py
+++ hugging_face_test.py
@@ -4,13 +4,7 @@ from transformers import pipeline
 # generator = pipeline('text-generation', model='./DialoGPT-medium')
 # print(generator('Tomorrow we will'))
 
-checkpoint = './DialoGPT-medium'
-tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-tokenizer.pad_token = tokenizer.eos_token
-raw_inputs = [
-    'This is input 1',
-    'Hello, I\'m the second input'
-]
-inputs = tokenizer.encode(raw_inputs)
-print(inputs)
-print(tokenizer.decode(inputs))
+tokenizer = AutoTokenizer.from_pretrained('./DialoGPT-medium')
+sequence = 'What is a token?'
+tokens = tokenizer.tokenize(sequence)
+print(tokens)
