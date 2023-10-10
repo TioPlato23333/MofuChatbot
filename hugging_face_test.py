c72c484b17ad915d91170344c40ddaf8a9bc2c77
TioPlato23333 <ningxiaodong23333@gmail.com>
2023-10-10 20:26:59 +0800

    [WIP] Try to generate sentences.

--- hugging_face_test.py
+++ hugging_face_test.py
@@ -1,3 +1,5 @@
+import torch
+from transformers import AutoModelForCausalLM
 from transformers import AutoTokenizer
 from transformers import pipeline
 
@@ -5,10 +7,12 @@ from transformers import pipeline
 # print(generator('Tomorrow we will'))
 
 tokenizer = AutoTokenizer.from_pretrained('./DialoGPT-medium')
+model = AutoModelForCausalLM.from_pretrained('./DialoGPT-medium')
 sequence = 'What is a token?'
-tokens = tokenizer.tokenize(sequence)
+tokens = tokenizer.tokenize(sequence, return_tensors='pt')
 print(tokens)
 ids = tokenizer.convert_tokens_to_ids(tokens)
 print(ids)
-decoded_string = tokenizer.decode(ids)
-print(decoded_string)
+output_ids = model.generate(torch.tensor([ids]), max_new_tokens=10, return_dict_in_generate=True)
+print(output_ids)
+print(tokenizer.decode(output_ids.sequences[0]))
