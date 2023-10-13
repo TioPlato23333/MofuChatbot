771b4a77d54b05b32c9dc2dce125386d9519c422
TioPlato23333 <ningxiaodong23333@gmail.com>
2023-10-13 19:28:48 +0800

    Try dataset.

--- hugging_face_test.py
+++ hugging_face_test.py
@@ -1,3 +1,4 @@
+from datasets import load_dataset
 import torch
 from transformers import AutoModelForCausalLM
 from transformers import AutoTokenizer
@@ -6,6 +7,7 @@ from transformers import pipeline
 # generator = pipeline('text-generation', model='./DialoGPT-medium')
 # print(generator('Tomorrow we will'))
 
+'''
 tokenizer = AutoTokenizer.from_pretrained('./DialoGPT-medium')
 model = AutoModelForCausalLM.from_pretrained('./DialoGPT-medium')
 sequence = 'What is a token?'
@@ -13,7 +15,10 @@ tokens = tokenizer.tokenize(sequence + tokenizer.eos_token, return_tensors='pt')
 print(tokens)
 ids = torch.tensor([tokenizer.convert_tokens_to_ids(tokens)])
 print(ids)
-output_ids = model.generate(ids, max_new_tokens=5, return_dict_in_generate=True, no_repeat_ngram_size=2,)
+output_ids = model.generate(ids, max_new_tokens=5, return_dict_in_generate=True, no_repeat_ngram_size=2)
 print(output_ids)
 print(ids.shape[-1])
 print(tokenizer.decode(output_ids.sequences[0][ids.shape[-1]:]))
+'''
+
+dataset = load_dataset('./lhoestq/demo1')
