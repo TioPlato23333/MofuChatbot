0889d0e39c9af5e49f05911a4682ecef170a8a9f
TioPlato23333 <ningxiaodong23333@gmail.com>
2023-10-17 18:51:10 +0800

    Start to study slanj's code.

---
+++ slanj_codes/main.py
@@ -0,0 +1,83 @@
+import glob
+import logging
+import os
+import pickle
+import random
+import re
+import shutil
+from typing import Dict, List, Tuple
+
+import pandas as pd
+import numpy as np
+import torch
+
+from sklearn.model_selection import train_test_split
+
+from torch.nn.utils.rnn import pad_sequence
+from torch.utils.data import DataLoader, Dataset, RandomSampler, SequentialSampler
+from torch.utils.data.distributed import DistributedSampler
+from tqdm.notebook import tqdm, trange
+
+from pathlib import Path
+
+from transformers import (
+    MODEL_WITH_LM_HEAD_MAPPING,
+    WEIGHTS_NAME,
+    AdamW,
+    AutoConfig,
+    AutoModelWithLMHead,
+    AutoTokenizer,
+    PreTrainedModel,
+    PreTrainedTokenizer,
+    get_linear_schedule_with_warmup,
+)
+
+
+try:
+    from torch.utils.tensorboard import SummaryWriter
+except ImportError:
+    from tensorboardX import SummaryWriter
+
+# Configs
+logger = logging.getLogger(__name__)
+
+MODEL_CONFIG_CLASSES = list(MODEL_WITH_LM_HEAD_MAPPING.keys())
+MODEL_TYPES = tuple(conf.model_type for conf in MODEL_CONFIG_CLASSES)
+
+class Args():
+    def __init__(self):
+        self.output_dir = 'output-small'
+        self.model_type = 'gpt2'
+        self.model_name_or_path = 'microsoft/DialoGPT-small'
+        self.config_name = 'microsoft/DialoGPT-small'
+        self.tokenizer_name = 'microsoft/DialoGPT-small'
+        self.cache_dir = 'cached'
+        self.block_size = 512
+        self.do_train = True
+        self.do_eval = True
+        self.evaluate_during_training = False
+        self.per_gpu_train_batch_size = 4
+        self.per_gpu_eval_batch_size = 4
+        self.gradient_accumulation_steps = 1
+        self.learning_rate = 5e-5
+        self.weight_decay = 0.0
+        self.adam_epsilon = 1e-8
+        self.max_grad_norm = 1.0
+        self.num_train_epochs = 3
+        self.max_steps = -1
+        self.warmup_steps = 0
+        self.logging_steps = 1000
+        self.save_steps = 3500
+        self.save_total_limit = None
+        self.eval_all_checkpoints = False
+        self.no_cuda = False
+        self.overwrite_output_dir = True
+        self.overwrite_cache = True
+        self.should_continue = False
+        self.seed = 42
+        self.local_rank = -1
+        self.fp16 = False
+        self.fp16_opt_level = 'O1'
+
+args = Args()
+print(args)
