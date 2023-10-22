2ca387730932ef7bc9c730dbf66c0dc476ebc75a
TioPlato23333 <ningxiaodong23333@gmail.com>
2023-10-22 20:53:58 +0800

    Refactor.

--- slanj_codes/main.py
+++ slanj_codes/main.py
@@ -83,21 +83,22 @@ class Args():
 
 args = Args()
 
-all_rick = pd.read_csv('RickAndMortyScripts.csv')
-contexted = []
-n = 7
-for i in range(n, len(all_rick['line'])):
-  row = []
-  prev = i - 1 - n # we additionally subtract 1, so row will contain current response and 7 previous responses  
-  for j in range(i, prev, -1):
-    row.append(all_rick['line'][j])
-  contexted.append(row)
-columns = ['response', 'context'] 
-columns = columns + ['context/' + str(i) for i in range(n - 1)]
-df = pd.DataFrame.from_records(contexted, columns=columns)
-df.head(5)
+def read_csv():
+    all_rick = pd.read_csv('RickAndMortyScripts.csv')
+    contexted = []
+    n = 7
+    for i in range(n, len(all_rick['line'])):
+      row = []
+      prev = i - 1 - n # we additionally subtract 1, so row will contain current response and 7 previous responses  
+      for j in range(i, prev, -1):
+        row.append(all_rick['line'][j])
+      contexted.append(row)
+    columns = ['response', 'context'] 
+    columns = columns + ['context/' + str(i) for i in range(n - 1)]
+    df = pd.DataFrame.from_records(contexted, columns=columns)
 
-trn_df, val_df = train_test_split(df, test_size=0.1)
+    trn_df, val_df = train_test_split(df, test_size=0.1)
+    return trn_df, val_df
 
 def construct_conv(row, tokenizer, eos=True):
     flatten = lambda l: [item for sublist in l for item in sublist]
@@ -131,4 +132,5 @@ class ConversationDataset(Dataset):
                 pickle.dump(self.examples, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_name, cache_dir=args.cache_dir)
+trn_df, val_df = read_csv()
 dataset = ConversationDataset(tokenizer, args, val_df)
